142f84d96e17bfd6180a485a0a86a70a3c022293
Nicholas <danielnick210@gmail.com>
2026-03-30 09:16:14 +0100

    Add .gitignore and README.md, update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv/config");
@@ -10,5 +11,36 @@ mongoose
 	.connect(MONGO_URI)
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.log(err));
+
+const expenseSchema = new mongoose.Schema({
+	title: { type: String, required: true },
+	amount: { type: Number, required: true },
+	category: { type: String, required: true },
+	date: { type: Date, default: Date.now },
+});
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+app.post("/expenses", async (req, res) => {
+	const { title, amount, category } = req.body;
+
+	try {
+		await Expense.create({ title, amount, category });
+	} catch (err) {
+		return res.json({ err });
+	}
+
+	res.json({ messages: "Creates successfully" });
+});
+
+app.get("/expenses", async (req, res) => {
+
+	try {
+		const result = await Expense.find();
+		res.json({ result });
+	} catch (err) {
+		return res.json({ err });
+	}
+});
 
 app.listen(3000, () => console.log("running"));
